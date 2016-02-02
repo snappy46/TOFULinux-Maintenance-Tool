a5df92da33afebdc4dd8637e2b6d900c0c235b5c
marcel <levesque.marcel@gmail.com>
2016-02-01 20:18:55 -0500

    Code clean up and python convention introduce into the code.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import urllib
 from xml.dom import minidom
 import xbmcgui
 import os
-import subprocess
 import hashlib
 from xbmcaddon import Addon
 import xbmc
@@ -13,56 +12,60 @@ linkArray = []
 md5Array = []
 webfile = ""
 
-addonPath=Addon().getAddonInfo('path')
-mediaPath=xbmc.translatePath(os.path.join(addonPath,'resources/media/'))
+addonPath = Addon().getAddonInfo('path')
+mediaPath = xbmc.translatePath(os.path.join(addonPath, 'resources/media/'))
 
 
-def mediaPathFile(file):
-    return mediaPath + file
+def media_path_file(media_file):
+    return mediaPath + media_file
 
 
-OverlayBackground = mediaPathFile('background.png')
-logoImage = mediaPathFile('Tofu-Linux-Logo-white.png')
+OverlayBackground = media_path_file('background.png')
+logoImage = media_path_file('Tofu-Linux-Logo-white.png')
 
 ACTION_PREVIOUS_MENU = 10
 
 
 class MainWindow(xbmcgui.Window):  # xbmcgui.Window): ##xbmcgui.Window
 
-    def __init__(self,  L=140,T=60,W=1000,H=560):
+    def __init__(self, l=140, t=60, w=1000, h=560):
+        super(MainWindow, self).__init__()
         self.background = OverlayBackground
-        self.BG = xbmcgui.ControlImage(L,T,W,H,self.background,aspectRatio=0) #,colorDiffuse='0xFF3030FF')
+        self.BG = xbmcgui.ControlImage(l, t, w, h, self.background, aspectRatio=0)  # ,colorDiffuse='0xFF3030FF')
         self.addControl(self.BG)
-        L2=320; T2=70; W2=640; H2=64
+        l2 = 320
+        t2 = 70
+        w2 = 640
+        h2 = 64
         self.logo = logoImage
-        self.LOGO = xbmcgui.ControlImage(L2, T2, W2, H2,logoImage,aspectRatio=0)
+        self.LOGO = xbmcgui.ControlImage(l2, t2, w2, h2, logoImage, aspectRatio=0)
         self.addControl(self.LOGO)
         self.show()
-        mainMenuSelection()
+        mainmenu_selection()
 
     def onAction(self, action):
-      if action == ACTION_PREVIOUS_MENU:
-        self.close()
+        if action == ACTION_PREVIOUS_MENU:
+            self.close()
 
 
-def langString(id):
+def lang_string(string_id):
     # Return string based on language and id
-    return Addon().getLocalizedString(id)
+    return Addon().getLocalizedString(string_id)
 
 
-def messageOK(message):
+def message_ok(message):
     # print dialogue window with OK button based on message
     dialog = xbmcgui.Dialog()
-    dialog.ok(langString(32002), message)
+    dialog.ok(lang_string(32002), message)
 
 
-def yesnoDialog(title, message):
+def yesno_dialog(title, message):
     # display yesno dialog and return selection (true=yes, false=no)
     dialog = xbmcgui.Dialog()
     return dialog.yesno(title, message)
 
 
-def findChildNodeByName(parent, name):
+def find_childnode_by_name(parent, name):
     # Return parent childnode that match name
     for node in parent.childNodes:
         if node.nodeType == node.ELEMENT_NODE and node.localName == name:
@@ -70,7 +73,7 @@ def findChildNodeByName(parent, name):
     return None
 
 
-def findStorageBasedOnDevice():
+def find_storage_based_on_device():
     # Return storage based on the device selected
     device = Addon().getSetting('device')
     if device == '1':
@@ -81,7 +84,7 @@ def findStorageBasedOnDevice():
         return str(int(Addon().getSetting('DSM1storage')) + 1)
 
 
-def downloadFirmwareList(source):
+def download_firmware_list(source):
     # Download firmware list and store relevant data in arrays
     try:
         response = urllib.urlopen(source)
@@ -89,31 +92,31 @@ def downloadFirmwareList(source):
         firmwares = doc.getElementsByTagName('Version')
 
         for firmware in firmwares:
-            versionDate = firmware.getAttribute('Updated')
-            basic = findChildNodeByName(firmware, 'Basic')
-            firmwareArray.append(basic.getAttribute('name') + langString(32003) + versionDate)
+            version_date = firmware.getAttribute('Updated')
+            basic = find_childnode_by_name(firmware, 'Basic')
+            firmwareArray.append(basic.getAttribute('name') + lang_string(32003) + version_date)
             linkArray.append(basic.getAttribute('URL'))
             md5Array.append(basic.getAttribute('MD5'))
 
     except:
-        messageOK(langString(32030))
+        message_ok(lang_string(32030))
 
 
-def firmwareDownloadLocation():
+def firmware_download_location():
     # return location to save the downloaded firmware and filename
-    downloadLocation = findStorageBasedOnDevice()
-    if downloadLocation == '0': #cache selected
+    download_location = find_storage_based_on_device()
+    if download_location == '0':  # cache selected
         return '/recovery/update.img'
-    elif downloadLocation == '1': #Sdcard selected
-        return mountLocation('/dev/cardblksd1') + '/update.img'
+    elif download_location == '1':  # Sdcard selected
+        return mount_location('/dev/cardblksd1') + '/update.img'
     else:
-        return mountLocation('/dev/sdb5') + '/marcel/update.img' #USB Storage
+        return mount_location('/dev/sdb5') + '/marcel/update.img'  # USB Storage
 
 
-def DownloaderClass(url, dest):
+def downloader(url, dest):
     # download file and display progress in dialog progress window
     dp = xbmcgui.DialogProgress()
-    dp.create(langString(32004), langString(32005), url)
+    dp.create(lang_string(32004), lang_string(32005), url)
     urllib.urlretrieve(url, dest, lambda nb, bs, fs, url=url: _pbhook(nb, bs, fs, url, dp))
 
 
@@ -128,105 +131,105 @@ def _pbhook(numblocks, blocksize, filesize, url=None, dp=None):
         dp.close()
     if dp.iscanceled():
         dp.close()
-        os.remove(firmwareDownloadLocation())
-        messageOK(langString(32031))
+        os.remove(firmware_download_location())
+        message_ok(lang_string(32031))
         quit()
 
 
-def firmwareLocationOnReboot():
+def firmware_location_onreboot():
     # return the firmware location on reboot recovery
-    rebootFirmwareLocation = findStorageBasedOnDevice()
-    if rebootFirmwareLocation == '0': #cache selected
+    reboot_firmware_location = find_storage_based_on_device()
+    if reboot_firmware_location == '0':  # cache selected
         return 'cache'
-    elif rebootFirmwareLocation == '1': #Sdcard selected
+    elif reboot_firmware_location == '1':  # Sdcard selected
         return 'sdcard'
     else:
         return 'udisk'
 
 
-def mountLocation(dev):
+def mount_location(dev):
     # This function will return the mount location of dev
     p = os.popen("df | grep '" + dev + "' | grep -oE '[^ ]+$' | head -n1")
     return p.read().replace("\n", "")
 
 
-def recoverCommand(menuSelection):
+def recover_command(menu_selection):
     # Construct recovery command and reboot pivos device
-    shellCommand = ':'
-    if menuSelection == 0 :
+    shell_command = ':'
+    if menu_selection == 0:
         # issue shell command to install selected firmware and reset to factory settings if specify in the setting file.
-        storage = firmwareLocationOnReboot()
+        storage = firmware_location_onreboot()
         if Addon().getSetting('factoryReset') == 'true':
-            shellCommand = 'echo -e "--update_package=/' + storage + '/update.img\n--wipe_cache\n--wipe_data" > /recovery/recovery/command || exit 1'
+            shell_command = 'echo -e "--update_package=/' + storage + '/update.img\n--wipe_cache\n--wipe_data" > /recovery/recovery/command || exit 1'
         else:
-            shellCommand = 'echo -e "--update_package=/' + storage + '/update.img\n--wipe_cache" > /recovery/recovery/command || exit 1'
-    elif menuSelection == 1:
-        if yesnoDialog("Pivos Maintenance", "Wipe Pivos device cache and data (Factory Reset)?"):
-            shellCommand = 'echo -e "--wipe_data\n--wipe_cache\n" > /recovery/recovery/command || exit 1'
-    elif menuSelection == 2:
-        if yesnoDialog("Pivos Maintenance Tool", "Wipe Pivos device cache?"):
-            shellCommand = 'echo -e "--wipe_cache\n" > /recovery/recovery/command || exit 1'
-    elif menuSelection == 3:
-        if yesnoDialog("Pivos Maintenance Tool", "Reboot Pivos device in recovery mode?"):
+            shell_command = 'echo -e "--update_package=/' + storage + '/update.img\n--wipe_cache" > /recovery/recovery/command || exit 1'
+    elif menu_selection == 1:
+        if yesno_dialog("Pivos Maintenance", "Wipe Pivos device cache and data (Factory Reset)?"):
+            shell_command = 'echo -e "--wipe_data\n--wipe_cache\n" > /recovery/recovery/command || exit 1'
+    elif menu_selection == 2:
+        if yesno_dialog("Pivos Maintenance Tool", "Wipe Pivos device cache?"):
+            shell_command = 'echo -e "--wipe_cache\n" > /recovery/recovery/command || exit 1'
+    elif menu_selection == 3:
+        if yesno_dialog("Pivos Maintenance Tool", "Reboot Pivos device in recovery mode?"):
             try:
                 os.remove('/recovery/recovery/command')
             except OSError:
                 pass
 
-    result = os.system(shellCommand)
-    if result == 0 and shellCommand != ':':
+    result = os.system(shell_command)
+    if result == 0 and shell_command != ':':
         dialog = xbmcgui.Dialog()
-        dialog.notification(langString(32009), langString(32016), icon=xbmcgui.NOTIFICATION_INFO, time=3000)
-        shellCommand = 'reboot recovery'
-        os.system(shellCommand)
+        dialog.notification(lang_string(32009), lang_string(32016), icon=xbmcgui.NOTIFICATION_INFO, time=3000)
+        shell_command = 'reboot recovery'
+        os.system(shell_command)
     if result != 0:
-        messageOK(langString(32033))
+        message_ok(lang_string(32033))
 
 
 def md5(fname):
     # return md5 of the file fname
-    dpMd5 = xbmcgui.DialogProgress()
-    message = langString(32017)
-    dpMd5.create(langString(32018), message)
-    fileSize = os.path.getsize(fname)
-    stepSize = round(fileSize/409600)
+    dp_md5 = xbmcgui.DialogProgress()
+    message = lang_string(32017)
+    dp_md5.create(lang_string(32018), message)
+    file_size = os.path.getsize(fname)
+    step_size = round(file_size / 409600)
     percent = 0
     count = 0
-    hash = hashlib.md5()
+    file_hash = hashlib.md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
-            hash.update(chunk)
-            dpMd5.update(percent)
+            file_hash.update(chunk)
+            dp_md5.update(percent)
             count += 1
-            if count > stepSize and percent < 100:
+            if count > step_size and percent < 100:
                 percent += 1
                 count = 0
-    dpMd5.close()
-    return hash.hexdigest()
+    dp_md5.close()
+    return file_hash.hexdigest()
 
 
-def firmwareUpdate(message,downloadLink,md5Sum):
+def firmware_update(message, download_link, md5_sum):
     # control the firmware selected installation
     dialog = xbmcgui.Dialog()
-    message = message + langString(32006)
-    runscript = dialog.yesno(langString(32002), message)
+    message = message + lang_string(32006)
+    runscript = dialog.yesno(lang_string(32002), message)
 
     if runscript and Addon().getSetting('factoryReset') == 'true':
-        runscript = dialog.yesno(langString(32007), langString(32008))
+        runscript = dialog.yesno(lang_string(32007), lang_string(32008))
 
     if runscript:
-        downloadFile = firmwareDownloadLocation()
-        DownloaderClass(downloadLink, downloadFile)
-        if md5(downloadFile) <> md5Sum:
+        download_file = firmware_download_location()
+        downloader(download_link, download_file)
+        if md5(download_file) != md5_sum:
             # display error message
-            messageOK(langString(32036))
+            message_ok(lang_string(32036))
         else:
             dialog = xbmcgui.Dialog()
-            dialog.notification(langString(32019), langString(32020), icon='', time=3000)
-            recoverCommand(0)
+            dialog.notification(lang_string(32019), lang_string(32020), icon='', time=3000)
+            recover_command(0)
 
 
-def checkHardware():
+def check_hardware():
     # Check hardware to determine correct firmware list link and device
     device = Addon().getSetting('device')
     if device == '1':
@@ -236,55 +239,45 @@ def checkHardware():
     elif device == '3':
         return 'http://update.pivosgroup.com/linux/m1/update.xml'
     else:
-        messageOK(langString(32035))
+        message_ok(lang_string(32035))
         Addon().openSettings()
         return ''
 
 
-def createMainMenuItems():
-    # Create menu items
-    mainMenuItems = []
-    mainMenuItems.append(langString(32051))
-    mainMenuItems.append(langString(32052))
-    mainMenuItems.append(langString(32053))
-    mainMenuItems.append(langString(32054))
-    mainMenuItems.append(langString(32055))
-    return mainMenuItems
-
-
-def mainMenuSelection():
+def mainmenu_selection():
+    """ Create main menu  """
+    mainmenu_items = [lang_string(32051), lang_string(32052), lang_string(32053), lang_string(32054), lang_string(32055)]
     while True:
-        #display menu in a dialogue for selection. selectedMenuItem = position of selection
-        selectedMenuItem = xbmcgui.Dialog().select(langString(32050), createMainMenuItems())
+        # display menu in a dialogue for selection. selectedMenuItem = position of selection
+        selected_menu_item = xbmcgui.Dialog().select(lang_string(32050), mainmenu_items)
 
-        if selectedMenuItem == 0: # Install firmware selected
-            #clear firmwareArray
+        if selected_menu_item == 0:  # Install firmware selected
+            # clear firmwareArray
             del firmwareArray[:]
             # firmware update.xml URL
-            imageListLink = checkHardware()
+            imagelist_link = check_hardware()
 
             # download firmware list for selection
-            if imageListLink != '':
-                downloadFirmwareList(imageListLink)
+            if imagelist_link != '':
+                download_firmware_list(imagelist_link)
 
                 # display firmware list in a dialogue for selection. ret = position of selection
-                ret = xbmcgui.Dialog().select(langString(32001), firmwareArray)  #
+                ret = xbmcgui.Dialog().select(lang_string(32001), firmwareArray)  #
 
-                if ret == -1: #no selection was made just quit
+                if ret == -1:  # no selection was made just quit
                     continue
                 else:
                     # proceed with firmware installation based on firmware selected.
-                    firmwareUpdate(firmwareArray[ret], linkArray[ret], md5Array[ret])
+                    firmware_update(firmwareArray[ret], linkArray[ret], md5Array[ret])
 
-        elif selectedMenuItem == 4 or selectedMenuItem == -1:  #exit or no selection made
+        elif selected_menu_item == 4 or selected_menu_item == -1:  # exit or no selection made
             break
         else:
-            recoverCommand(selectedMenuItem)
+            recover_command(selected_menu_item)
 
-# instanciate main window
-mydisplay = MainWindow()
-
-# remove main window
-del mydisplay
-
-quit()
+if __name__ == "__main__":
+    # instanciate main window
+    mydisplay = MainWindow()
+    # remove main window
+    del mydisplay
+    quit()
